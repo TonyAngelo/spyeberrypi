230fc206be4b95ccb92332096f97beb9338caeca
Tony Petrangelo <tony.petrangelo@spye.co>
2016-05-24 10:03:39 -0500

    tweeked models file for spye-pir

--- models/models.py
+++ models/models.py
@@ -31,7 +31,7 @@ class Observable:
 
 from pir import Sensor
 from spye import Spyeworks
-from ipscan import find_mac_on_network
+#from ipscan import find_mac_on_network
 
 # model
 class Model:
@@ -42,31 +42,26 @@ class Model:
         # problem opening the file, load the default values
         except:
             logger.warn("Could not open spyeconfig.txt")
-            
-            self.ipaddy = Observable("192.168.1.110")
-            self.mac = Observable("00:00:00:00:00:00")
+
             self.filepath = Observable("c:/users/public/documents/spyeworks/content/")
+            self.ipaddy = Observable("192.168.1.110")
+            #self.mac = Observable("00:00:00:00:00:00")
             self.active = Observable("active")
+            self.activedelaytime = Observable("10")
             self.idle = Observable("idle")
-            self.sensorenable = Observable("T")
-            self.activelist = Observable("T")
-            self.activedelaytime = Observable("0")
-            self.idlelist = Observable("T")
-            self.idledelaytime = Observable("0")
             self.UpdateTextFile()
+
+            logger.warn("spyeconfig.txt created with default values.")
         else:
             logger.info("Parsing spyeconfig.txt...")
 
-            self.mac = Observable(f.readline()[:-1])
-            self.ipaddy = Observable(find_mac_on_network(self.mac.get()))
             self.filepath = Observable(f.readline()[:-1])
+            #self.mac = Observable(f.readline()[:-1])
+            #self.ipaddy = Observable(find_mac_on_network(self.mac.get()))
+            self.ipaddy = Observable(f.readline()[:-1])
             self.active = Observable(f.readline()[:-1])
-            self.idle = Observable(f.readline()[:-1])
-            self.sensorenable = Observable(f.readline()[:-1])
-            self.activelist = Observable(f.readline()[:-1])
             self.activedelaytime = Observable(f.readline()[:-1])
-            self.idlelist = Observable(f.readline()[:-1])
-            self.idledelaytime = Observable(f.readline()[:-1])
+            self.idle = Observable(f.readline()[:-1])
 
             logger.info("Parsing complete.")
         # close the file
@@ -76,8 +71,8 @@ class Model:
         self.sensorstate = Sensor(14)
 
         # initiate the spyeworks player
-        if len(self.ipaddy.get())==0:
-            logger.error("No IP address found for MAC %s" , self.mac.get())
+        #if len(self.ipaddy.get())==0:
+            #logger.error("No IP address found for MAC %s" , self.mac.get())
 
         self.spyeworks = Spyeworks(self.ipaddy.get(),self.filepath.get(),
                                    self.active.get(),self.idle.get())
@@ -110,24 +105,8 @@ class Model:
         # also update the spyeworks player
         self.spyeworks.idle=value
 
-    def SetSensorEnable(self,value):
-        self.sensorenable.set(value)
-        self.UpdateTextFile()
-
-    def SetActiveList(self, value):
-        self.activelist.set(value)
-        self.UpdateTextFile()
-
     def SetActiveDelayTime(self, value):
         self.activedelaytime.set(value)
-        self.UpdateTextFile()
-
-    def SetIdleList(self, value):
-        self.idlelist.set(value)
-        self.UpdateTextFile()
-
-    def SetIdleDelayTime(self, value):
-        self.idledelaytime.set(value)
         self.UpdateTextFile()
 
     ##########################################################
@@ -138,7 +117,6 @@ class Model:
         logger.info("Writing to spyeconfig.txt...")
         # write the model to a text file for tracking variable changes
         f=open('spyeconfig.txt','w+')
-        f.write(self.mac.get()+'\n'+self.filepath.get()+'\n'+self.active.get()+'\n'+self.idle.get()+'\n'+self.sensorenable.get()+'\n'+
-            self.activelist.get()+'\n'+self.activedelaytime.get()+'\n'+self.idlelist.get()+'\n'+self.idledelaytime.get()+'\n')
+        f.write(self.filepath.get() + '\n' + self.ipaddy.get() + '\n' + self.active.get() + '\n' + self.activedelay.get() + '\n' + self.idle.get() + '\n')
         f.close()
         logger.info("Writing complete.")

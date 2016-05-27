582f60289eae75f43f34f83b91d0f9079261a20e
Tony Petrangelo <tony.petrangelo@spye.co>
2016-05-27 14:57:00 -0500

    added days change functionality

--- tests/timer-test.py
+++ tests/timer-test.py
@@ -7,6 +7,12 @@ from apscheduler.scheduler import Scheduler
 logging.basicConfig(format='%(asctime)s %(levelname)-5s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename='logs/models.log', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+dayLabels=['Daily','WeekDays']
+dayOptions={
+    'Daily':'mon-sun',
+    'WeekDays':'mon-fri'
+}
+
 # data object
 class Observable:
     def __init__(self, initialValue=None):
@@ -43,7 +49,8 @@ class Controller:
 
         # set default turn on and turn off times
         # default to everyday
-        self.days='mon-sun'
+        self.daysLabel='Daily'
+        self.days=dayOptions[self.daysLabel]
         # turn on at 7am
         self.turnOnHour = 7
         self.turnOnMin = 0
@@ -61,10 +68,11 @@ class Controller:
         print("""
         Timer Test Menu
 
-            1. Set Turn On Time
-            2. Set Turn Off Time
-            3. Get On-Off Times
-            4. Quit/Exit
+            1. Set Turn On/Off Days
+            2. Set Turn On Time
+            3. Set Turn Off Time
+            4. Get On-Off Times
+            5. Quit/Exit
             """)
 
         # get the selection
@@ -72,6 +80,19 @@ class Controller:
         print("\n")
 
         if self.main_selection == '1':
+            print('Current Turn On/Off days:',self.daysLabel)
+            print('1. Daily')
+            print('2. WeekDays')
+            self.newDays = input("Select which days to use: ")
+            # validate entry
+            if int(self.newDays)==1 or int(self.newDays)==2:
+                self.daysLabel = dayLabels[int(self.newDays)]
+                self.days = dayOptions[self.daysLabel]
+                print('New Turn On/Off days:', self.daysLabel)
+            else:
+                print('Invalid entry')
+            self.printMenu()
+        elif self.main_selection == '2':
             print('Current Turn On time ', str(self.turnOnHour), ':', str(self.turnOnMin).zfill(2), sep='')
             self.newTurnOnHour = input("Enter new turn on hour (in 24 hour clock): ")
             # validate hour entry
@@ -80,9 +101,9 @@ class Controller:
                 # validate min entry
                 if int(self.newTurnOnMin) < 60 and int(self.newTurnOnMin) >= 0:
                     # assign new hour
-                    self.turnOnHour = self.newTurnOnHour
+                    self.turnOnHour = int(self.newTurnOnHour)
                     # assign new minute
-                    self.turnOnMin = self.newTurnOnMin
+                    self.turnOnMin = int(self.newTurnOnMin)
                     # cancel the old job
                     self.sched.unschedule_job(self.DisplayOnJob)
                     # schedule the new job
@@ -95,7 +116,7 @@ class Controller:
             else:
                 print('Invalid Turn On Hour')
             self.printMenu()
-        elif self.main_selection == '2':
+        elif self.main_selection == '3':
             print('Current Turn Off time ', str(self.turnOffHour), ':', str(self.turnOffMin).zfill(2), sep='')
             self.newTurnOffHour = input("Enter new turn off hour (in 24 hour clock): ")
             # validate hour entry
@@ -104,9 +125,9 @@ class Controller:
                 # validate min entry
                 if int(self.newTurnOffMin) < 60 and int(self.newTurnOffMin) >= 0:
                     # assign new hour
-                    self.turnOffHour = self.newTurnOffHour
+                    self.turnOffHour = int(self.newTurnOffHour)
                     # assign new minute
-                    self.turnOffMin = self.newTurnOffMin
+                    self.turnOffMin = int(self.newTurnOffMin)
                     # cancel the old job
                     self.sched.unschedule_job(self.DisplayOffJob)
                     # schedule the new job
@@ -119,12 +140,12 @@ class Controller:
             else:
                 print('Invalid Turn Off Hour')
             self.printMenu()
-        elif self.main_selection == '3':
-            print('Turn On Daily at ',str(self.turnOnHour),':',str(self.turnOnMin).zfill(2), sep='')
-            print('Turn Off Daily at ', str(self.turnOffHour), ':', str(self.turnOffMin).zfill(2), sep='')
+        elif self.main_selection == '4':
+            print('Turn On ',self.daysLabel,' at ',str(self.turnOnHour),':',str(self.turnOnMin).zfill(2), sep='')
+            print('Turn Off ',self.daysLabel,' at ', str(self.turnOffHour), ':', str(self.turnOffMin).zfill(2), sep='')
             self.sched.print_jobs()
             self.printMenu()
-        elif self.main_selection == '4':
+        elif self.main_selection == '5':
             sys.exit()
         else:
             print("Invalid selection.\n")
